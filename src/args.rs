4886b6379bd129afe2278b23accfd215de1dbb56
Akira Ueda <ue@gol.com>
2021-07-28 08:44:51 +0900

    allow args

--- src/args.rs
+++ src/args.rs
@@ -3,5 +3,6 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "main")]
 pub struct Opt {
-
+    #[structopt(name = "ARG")]
+    pub args: Vec<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use structopt::StructOpt;
 mod args;
 
 fn main() {
+    let envvars: Vec<(String, String)> = std::env::vars().collect();
+    let envvars: Vec<(String, String)> = envvars.into_iter().filter(|(k, _)| k.starts_with("GIT_")).collect();
+    eprintln!("env =============================");
+    eprintln!("{:#?}", envvars);
+
     let opt = Opt::from_args();
     eprintln!("args ============================");
     eprintln!("{:#?}", opt);
-
-    let envvars: Vec<(String, String)> = std::env::vars().collect();
-    eprintln!("env =============================");
-    eprintln!("{:#?}", envvars);
 }
