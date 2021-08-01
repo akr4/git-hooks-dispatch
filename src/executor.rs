f393ed2b3a81cd9dd4950e4a8e37063f6fd4a336
Akira Ueda <ue@gol.com>
2021-08-01 09:39:35 +0900

    Fix infinite execution

--- src/executor.rs
+++ src/executor.rs
@@ -20,7 +20,10 @@ impl ExecutorImpl {
 
 impl Executor for ExecutorImpl {
     fn execute(&self, working_dir: &Path, hook_path: &Path, args: &Vec<String>) -> Result<i32> {
-        let output = Command::new(hook_path).args(args).current_dir(working_dir).output()?;
+        let output = Command::new(hook_path)
+            .args(args)
+            .current_dir(working_dir)
+            .output()?;
         std::io::stdout().write_all(&output.stdout).unwrap();
         std::io::stderr().write_all(&output.stderr).unwrap();
         Ok(output.status.code().unwrap_or(1))
--- src/run.rs
+++ src/run.rs
@@ -36,6 +36,10 @@ pub fn run<P: AsRef<Path>, E: Executor>(
             if !abs_path.exists() {
                 continue;
             }
+            if abs_path.canonicalize()? == repo_dir.as_ref().canonicalize()? {
+                log::debug!("found the repo dir");
+                break;
+            }
             log::debug!(
                 "searching hook in {}",
                 abs_path
@@ -256,6 +260,52 @@ mod tests {
         )?;
 
         assert_eq!(status_code, 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn should_not_execute_hook_in_root_directory() -> Result<()> {
+        let repo_dir = TempDir::new("repo")?;
+
+        std::fs::create_dir(repo_dir.path().join("git-hooks"))?;
+        std::fs::File::create(repo_dir.path().join("git-hooks/pre-commit"))?;
+
+        std::fs::create_dir(repo_dir.path().join("1"))?;
+        std::fs::create_dir(repo_dir.path().join("1/git-hooks"))?;
+
+        let a = Path::new("1/a");
+        std::fs::File::create(repo_dir.path().join(a))?.write_all("a".as_bytes())?;
+
+        let pre_commit_abs_path = repo_dir.path().join("1/git-hooks/pre-commit");
+        std::fs::File::create(pre_commit_abs_path.as_path())?;
+
+        let repo = git2::Repository::init(repo_dir.path()).unwrap();
+        let mut index = repo.index()?;
+        index.add_all(vec![a], git2::IndexAddOption::DEFAULT, None)?;
+        index.write()?;
+
+        let working_dir_abs_path = repo_dir.path().join("1");
+
+        let mut mock = MockExecutor::new();
+        mock.expect_execute()
+            .withf(
+                move |working_dir: &Path, hook_path: &Path, args: &Vec<String>| {
+                    working_dir == working_dir_abs_path
+                        && hook_path == pre_commit_abs_path
+                        && args.len() == 0
+                },
+            )
+            .times(1)
+            .returning(|_, _, _| Ok(0));
+
+        run(
+            repo_dir.path(),
+            "pre-commit",
+            &vec![],
+            &mock,
+            vec!["git-hooks".to_string()],
+        )?;
 
         Ok(())
     }
